62409056eec0eac138c9e056fab0cdb959682a62
LeHongTan <lh.tan6002@gmail.com>
2025-12-30 15:38:12 +0700

    creating get_data thread [ERROR - fixing]

---
+++ build/CMakeFiles/iris.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iris.dir/main.cpp.o"
+  "CMakeFiles/iris.dir/main.cpp.o.d"
+  "CMakeFiles/iris.dir/src/coin.cpp.o"
+  "CMakeFiles/iris.dir/src/coin.cpp.o.d"
+  "CMakeFiles/iris.dir/src/utils.cpp.o"
+  "CMakeFiles/iris.dir/src/utils.cpp.o.d"
+  "iris"
+  "iris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
